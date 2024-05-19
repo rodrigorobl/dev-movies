32811c6643daed76024573199bd1caaa39a4124c
rodrigorobl <rodrigo.augusto.r21@gmail.com>
2024-05-19 13:11:30 -0300

    dev-movies19052024

--- src/containers/Home/index.jsx
+++ src/containers/Home/index.jsx
@@ -1,5 +1,5 @@
 import api from "../../services/api";
-import { Background } from "./styles";
+import { Background, Container, Info, Poster } from "./styles";
 import { useEffect, useState } from "react";
 
 function Home() {
@@ -11,7 +11,7 @@ function Home() {
         data: { results },
       } = await api.get("/movie/popular");
 
-      setMovie(results[0]);
+      setMovie(results[1]);
     }
 
     console.log(movie);
@@ -24,8 +24,15 @@ function Home() {
         <Background
           img={`https://image.tmdb.org/t/p/original${movie.backdrop_path}`}
         >
-          <h1>{movie.title}</h1>
-          <p>{movie.overview}</p>
+          <Container>
+            <Info>
+              <h1>{movie.title}</h1>
+              <p>{movie.overview}</p>
+            </Info>
+            <Poster>
+              <img alt="capa-do-filme" src={`https://image.tmdb.org/t/p/original${movie.poster_path}`}/>
+            </Poster>
+          </Container>
         </Background>
       )}
     </>

d9196bede17e11e25c9a434e580c5bc87aee1ee2
rodrigorobl <rodrigo.augusto.r21@gmail.com>
2024-06-02 13:25:01 -0300

    dev-movie020624-2

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,12 +1,39 @@
-import Logo from '../../assets/logo.png'
+import { Link, useLocation } from "react-router-dom";
 
-function Header(){
+import Logo from "../../assets/logo.png";
 
-    return (
-        <div>
-            <img src={Logo} alt='logo-dev-movies' style={{ width: 500}} />
-        </div>
-    )
+import { Container, Menu, Li } from "./styles";
+import { useState } from "react";
+
+function Header() {
+    const [changeBackground, setChangeBackground] = useState(false)
+  const { pathname } = useLocation();
+
+  window.onscroll = () => {
+    if(!changeBackground && window.pageYOffset > 150) {
+        setChangeBackground(true)
+    }
+    if(changeBackground && window.pageYOffset <= 150) {
+        setChangeBackground(false)
+    }
+  }
+
+  return (
+    <Container changeBackground={changeBackground}>
+      <img src={Logo} alt="logo-dev-movies" />
+      <Menu>
+        <Li isActive={pathname === '/'}>
+          <Link to="/">Home</Link>
+        </Li>
+        <Li isActive= {pathname.includes('filmes')}>
+          <Link to="/filmes">Filmes</Link>
+        </Li>
+        <Li isActive={pathname.includes('series')}>
+          <Link to="/series">Séries</Link>
+        </Li>
+      </Menu>
+    </Container>
+  );
 }
 
-export default Header
+export default Header;
--- src/layout/DefaultLayout.jsx
+++ src/layout/DefaultLayout.jsx
@@ -1,15 +1,16 @@
-import { Outlet } from 'react-router-dom'
+import { Outlet } from "react-router-dom";
 
-//import Header from '../components/Header'
+import Header from "../components/Header";
+
+
 
 function DefaultLayout() {
-
-    return (
-        <>
-            {/* <Header /> */}
-            <Outlet />
-        </>
-    )
+  return (
+    <>
+      <Header />
+      <Outlet />
+    </>
+  );
 }
 
-export default DefaultLayout
+export default DefaultLayout;
